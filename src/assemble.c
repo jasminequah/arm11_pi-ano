9e03e3ffd03e284b19cb914204ecb86b0498ac6c
Bargavi Chandramouli <bc1417@ic.ac.uk>
2018-06-01 15:03:13 +0100

    added a readfile method that needs to be tested

--- src/assemble.c
+++ src/assemble.c
@@ -2,11 +2,52 @@
 #include <stdint.h>
 
 typedef struct Map {
-	char label[];
+	char label[511];
 	uint16_t memAddress;
 }map;
 
+typedef struct instruction {
+	/*instruction, rd, rn, rs, operand2, rm*/
+}instr;
+
 typedef map* symbolTable[];
+
+/*sample readfile statement*/
+
+char** readfile(FILE *fp) {
+	/*gets file size*/
+	int i = 0;
+	int size = 0;
+
+	do {
+		char c[511];
+		fgets(c, 511, fp);
+		//fseek(fp, ftell(fp)+1, SEEK_SET);
+		size++;
+		i++;
+	} while (feof(fp) == 0);
+
+	//printf("%d \n", size);
+
+	fseek(fp, 0, SEEK_SET);
+
+	/* 
+	Stores instructions in heap
+	Assumes instruction size is less tha 511
+	*/
+
+	/*Allocates memory*/
+	char **memInstr = (char**)malloc(size);
+	for (int i = 0; i < size; i++) {
+		memInstr[i] = (char*)malloc(511);
+	}
+
+	for (int i = 0; i < size; i++) {
+		fgets(memInstr[i], 511, fp);
+	}
+
+	return memInstr;
+}
 
 /* First Pass: Makes symbol table- 
         1)Associate each label(string) with a memory address(integer)
