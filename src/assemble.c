36691332d74ed00206a2bef93bff9cae46ec1718
Jasmine Quah <jlq17@matrix10.doc.ic.ac.uk>
2018-06-06 23:03:39 +0100

    Completed optional case for parseDataProcessing, got rid of magic numbers in code

--- src/assemble.c
+++ src/assemble.c
@@ -9,12 +9,23 @@
 #define ADDR_INC              0x4
 #define MAX_SYMBOL_TABLE_SIZE 50
 #define BITS_IN_WORD          32
+#define HEX_BASE              16
+#define DECIMAL_BASE          10
 
 #define S_BIT             0x00080000
 #define I_BIT             0x02000000
 #define ALWAYS_COND_CODE  0xe0000000
 #define MAX_NO_ROTATE_IMM 0xff
 #define MAX_ROTATE        0xf
+
+#define OPCODE_SHIFT         21
+#define ROTATE_SHIFT         8
+#define SHIFT_BY_REG_BIT     0x00000010
+#define RN_SHIFT             16
+#define RD_SHIFT             12
+#define RS_SHIFT             8
+#define SHIFT_CONSTANT_SHIFT 8
+#define SHIFT_TYPE_SHIFT     5
 
 typedef struct map {
   char *label;
@@ -34,10 +45,10 @@ uint32_t getOperand(char *expression) {
   char *endPtr;
   if (strncmp("0x", expression, 2) == 0) {
     // Numeric constant in hexadecimal
-    return strtol(expression + 2 * (sizeof(char)), &endPtr, 16);
+    return strtol(expression + 2 * (sizeof(char)), &endPtr, HEX_BASE);
   } else {
     // Numeric constant in decimal
-    return strtol(expression, &endPtr, 10);
+    return strtol(expression, &endPtr, DECIMAL_BASE);
   }
 }
 
@@ -57,68 +68,90 @@ void getImmediate(uint32_t *instrPtr, uint32_t operand2) {
     operand2 = rotateLeftByTwo(operand2);
   }
   
-  *instrPtr = *instrPtr | (rotateAmount << 8) | operand2;
+  *instrPtr = *instrPtr | (rotateAmount << ROTATE_SHIFT) | operand2;
 }
 
-void setReg(dataProcType_t instrType, char **tokens, uint32_t *instrPtr) {
+void getExprBits(uint32_t *instrPtr, char *expression) {
+  if (expression[0] == '#') {
+    *instrPtr = *instrPtr | I_BIT;
+  }
+  uint32_t operand2 = getOperand(expression + sizeof(char));
+  getImmediate(instrPtr, operand2);
+
+}
+
+void getShiftBits(uint32_t *instrPtr, char **remainingTokens) {
+
+  uint32_t rm = atoi(&remainingTokens[0][1]);
+  uint32_t shiftType;
+  
+  if (strcmp(remainingTokens[1], "lsl") == 0) {
+    shiftType = 0;
+  } else if (strcmp(remainingTokens[1], "lsr") == 0) {
+    shiftType = 1;
+  } else if (strcmp(remainingTokens[1], "asr") == 0) {
+    shiftType = 2;
+  } else if (strcmp(remainingTokens[1], "ror") == 0) {
+    shiftType = 3;
+  } else {
+    printf("Error: unrecognised shift type\n");
+  }
+
+  if (remainingTokens[2][0] == '#') {
+    uint32_t shiftAmount = getOperand(remainingTokens[2] + sizeof(char));
+    *instrPtr = *instrPtr | (shiftAmount << SHIFT_CONSTANT_SHIFT);
+  } else {
+    uint32_t rs = atoi(&remainingTokens[2][1]);
+    *instrPtr = *instrPtr | (rs << RS_SHIFT) | SHIFT_BY_REG_BIT;
+  }
+
+  *instrPtr = *instrPtr | (shiftType << SHIFT_TYPE_SHIFT) | rm;
+
+}
+
+void setReg(dataProcType_t instrType, char **tokens, uint32_t *instrPtr, int numTokens) {
   uint32_t rd;
   uint32_t rn;
-  uint32_t operand2;
-
   switch(instrType) {
     case COMP_RESULT:
-      rd          = atoi(&tokens[1][1]);
-      rn          = atoi(&tokens[2][1]);
-
-      if (tokens[3][0] == '#') {
-        *instrPtr = *instrPtr | I_BIT;
+      rd = atoi(&tokens[1][1]);
+      rn = atoi(&tokens[2][1]);
+      
+      if (numTokens == 4) {
+        // Expression
+        getExprBits(instrPtr, tokens[3]);
+      } else {
+        // Shifted register 
+        getShiftBits(instrPtr, &tokens[3]);
       }
-
-      operand2    = getOperand(tokens[3] + sizeof(char));
-      getImmediate(instrPtr, operand2);
-
-      *instrPtr   = *instrPtr | (rn << 16) | (rd << 12);
+      
+      *instrPtr = *instrPtr | (rn << RN_SHIFT) | (rd << RD_SHIFT);
       break;
+
     case SINGLE_OP_ASS:
-      rd          = atoi(&tokens[1][1]);
-
-      if (tokens[2][0] == '#') {
-        *instrPtr = *instrPtr | I_BIT;
+      rd = atoi(&tokens[1][1]);
+      if (numTokens == 3) {
+        getExprBits(instrPtr, tokens[2]);
+      } else {
+        getShiftBits(instrPtr, &tokens[2]);
       }
 
-      operand2    = getOperand(tokens[2] + sizeof(char));
-      getImmediate(instrPtr, operand2);
-
-      *instrPtr   = *instrPtr | (rd << 12);
+      *instrPtr = *instrPtr | (rd << RD_SHIFT);
       break;
+
     case SET_CPSR:
-      rn          = atoi(&tokens[1][1]);
-
-      if (tokens[2][0] == '#') {
-        *instrPtr = *instrPtr | I_BIT;
+      rn = atoi(&tokens[1][1]);
+      if (numTokens == 3) {
+        getExprBits(instrPtr, tokens[2]);
+      } else {
+        getShiftBits(instrPtr, &tokens[2]);
       }
 
-      operand2     = getOperand(tokens[2] + sizeof(char));
-      getImmediate(instrPtr, operand2);       
-
-      *instrPtr    = *instrPtr | (rn << 16);
+      *instrPtr = *instrPtr | (rn << RN_SHIFT);
   }
 }
 
-uint32_t getMemAddress(map_t *symbolTable, char *label) {
-  map_t *currMap = symbolTable;
-  while (1) {
-    if (strcmp(currMap->label, label) == 0) {
-      break;
-    }
-    currMap++;
-  }
-  printf("========= exits getMemAddress while =========\n");
-  return currMap->memAddress;
-}
-
-/* Does not support case when operand2 is a shift so sets I bit */
-uint32_t parseDataProcessing(map_t *symbolTable, char **tokens, instrName_t name) {
+uint32_t parseDataProcessing(map_t *symbolTable, char **tokens, instrName_t name, int numTokens) {
   uint32_t opCode;
 
   // Set condition to always
@@ -127,45 +160,45 @@ uint32_t parseDataProcessing(map_t *symbolTable, char **tokens, instrName_t name
   switch(name) {
     case AND:
       opCode = 0;
-      setReg(COMP_RESULT, tokens, &instruction);
+      setReg(COMP_RESULT, tokens, &instruction, numTokens);
       break;
     case EOR:
       opCode = 1;
-      setReg(COMP_RESULT, tokens, &instruction);
+      setReg(COMP_RESULT, tokens, &instruction, numTokens);
       break;
     case SUB:
       opCode = 2;
-      setReg(COMP_RESULT, tokens, &instruction);
+      setReg(COMP_RESULT, tokens, &instruction, numTokens);
       break;
     case RSB:
       opCode = 3;
-      setReg(COMP_RESULT, tokens, &instruction);
+      setReg(COMP_RESULT, tokens, &instruction, numTokens);
       break;
     case ADD:
       opCode = 4;
-      setReg(COMP_RESULT, tokens, &instruction);
+      setReg(COMP_RESULT, tokens, &instruction, numTokens);
       break;
     case ORR:
       opCode = 12;
-      setReg(COMP_RESULT, tokens, &instruction);
+      setReg(COMP_RESULT, tokens, &instruction, numTokens);
       break;
     case MOV:
       opCode = 13;
-      setReg(SINGLE_OP_ASS, tokens, &instruction);
+      setReg(SINGLE_OP_ASS, tokens, &instruction, numTokens);
       break;
     case TST:
       opCode = 8;
-      setReg(SET_CPSR, tokens, &instruction);
+      setReg(SET_CPSR, tokens, &instruction, numTokens);
       instruction = instruction | S_BIT;
       break;
     case TEQ:
       opCode = 9;
-      setReg(SET_CPSR, tokens, &instruction);
+      setReg(SET_CPSR, tokens, &instruction, numTokens);
       instruction = instruction | S_BIT;
       break;
     case CMP:
       opCode = 10;
-      setReg(SET_CPSR, tokens, &instruction);
+      setReg(SET_CPSR, tokens, &instruction, numTokens);
       instruction = instruction | S_BIT;
       break;
     default:
@@ -173,9 +206,10 @@ uint32_t parseDataProcessing(map_t *symbolTable, char **tokens, instrName_t name
       opCode = -1;
       break;
   }
-  instruction = instruction | (opCode << 21);
+  instruction = instruction | (opCode << OPCODE_SHIFT);
   return instruction;
 }
+
 
 
 uint32_t parseSDT(map_t *symbolTable, char **tokens, instrName_t name) {
@@ -289,6 +323,19 @@ uint32_t parseMultiply(map_t *symbolTable, char **tokens, instrName_t name) {
 	return code;
 }
 
+uint32_t getMemAddress(map_t *symbolTable, char *label) {
+  map_t *currMap = symbolTable;
+  while (1) {
+    printf("label in map: %s, label: %s\n", currMap->label, label);
+    if (strcmp(currMap->label, label) == 0) {
+      break;
+    }
+    currMap++;
+  }
+  printf("========= exits getMemAddress while =========\n");
+  return currMap->memAddress;
+}
+
 uint32_t parseBranch(map_t *symbolTable, char **tokens, instrName_t name, int currAddress) {
   uint32_t cond;
     switch(name) {
@@ -314,7 +361,7 @@ uint32_t parseBranch(map_t *symbolTable, char **tokens, instrName_t name, int cu
         cond = 14;
         break;
       default:
-        // Should return an error
+        printf("Error: invalid condition\n");
         cond = -1;
         break;
     }
@@ -494,34 +541,34 @@ void secondPass(char *fileName, map_t *symbolTable, uint32_t *binaryInstructions
       instrName_t instrName = toInstrName(tokens[0]);
       switch (instrName) {
         case ADD :
-      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, ADD);
+      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, ADD, i);
       	  break;
       	case SUB :
-      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, SUB);
+      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, SUB, i);
       	  break;
       	case RSB :
-      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, RSB);
+      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, RSB, i);
       	  break;
       	case AND :
-      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, AND);
+      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, AND, i);
       	  break;
       	case EOR :
-      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, EOR);
+      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, EOR, i);
       	  break;
       	case ORR :
-      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, ORR);
+      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, ORR, i);
       	  break;
       	case MOV :
-      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, MOV);
+      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, MOV, i);
       	  break;
       	case TST :
-      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, TST);
+      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, TST, i);
       	  break;
       	case TEQ :
-      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, TEQ);
+      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, TEQ, i);
       	  break;
       	case CMP :
-      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, CMP);
+      	  binaryInstructions[instrNum] = parseDataProcessing(symbolTable, tokens, CMP, i);
       	  break;
       	case MUL :
       	  binaryInstructions[instrNum] = parseMultiply(symbolTable, tokens, MUL);
