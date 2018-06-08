9527f82ba4f22ebe8e569e8b0a466f4e576f2743
Bargavi Chandramouli <bc1417@ic.ac.uk>
2018-06-08 13:37:18 +0100

    an attempt at gpio

---
+++ programs/gpio.s
@@ -0,0 +1,23 @@
+mov r1, #0x00010000
+mov r2, #0x20200028
+str r1, [r2]
+mov r3, #0x2020001C
+str r1, [r3]
+mov r5, #10
+mainloop:
+mov r4, #50
+loop:
+str r1, [r3]
+sub r4, #1
+cmp r4, #0
+blt loop
+str r1, [r2]
+mov r4, #50
+loop2:
+str r1, [r2]
+sub r4, #1
+cmp r4, #0
+blt loop2
+sub r5, #1
+cmp r5, #0
+blt mainloop
