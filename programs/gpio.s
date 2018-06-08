4141267e3af115968f65cecc7c63bd567ab11b6a
Bargavi Chandramouli <bc1417@ic.ac.uk>
2018-06-08 15:20:20 +0100

    gpio still not working

--- programs/gpio.s
+++ programs/gpio.s
@@ -7,6 +7,7 @@ str r1, [r2]
 ldr r3, =0x2020001C
 str r1, [r3]
 mov r5, #100
+mov r7, #0
 mainloop:
 mov r4, #0xff
 loop:
@@ -14,13 +15,12 @@ str r1, [r3]
 sub r4, r4, #1
 cmp r4, #0
 blt loop
-str r1, [r2]
+str r7, [r3]
 mov r4, #0xff
 loop2:
-str r1, [r2]
+str r7, [r3]
 sub r4, r4, #1
 cmp r4, #0
 blt loop2
-sub r5, r5, #1
 cmp r5, #0
 blt mainloop
