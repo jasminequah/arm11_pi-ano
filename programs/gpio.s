3752f44faeb7d3c9c7e3a7abbb5536ac698eaaef
Jasmine Quah <jlq17@edge38.doc.ic.ac.uk>
2018-06-08 16:13:19 +0100

    Esther: increased time between flashes

--- programs/gpio.s
+++ programs/gpio.s
@@ -1,21 +1,20 @@
-ldr r0 =0x20200004
-mov r6, #0x00040000
+ldr r0, =0x20200004
+ldr r6, =0x00040000
 str r6, [r0]
-mov r1, #0x00010000
+ldr r1, =0x00010000
 ldr r2, =0x20200028
 str r1, [r2]
 ldr r3, =0x2020001C
 str r1, [r3]
-ldr r5, =0xfffff
 mainloop:
-ldr r4, =0xfffff
+ldr r4, =0xffffff
 loop:
 str r1, [r3]
 sub r4, r4, #1
 cmp r4, #0
 blt loop
 str r6, [r2]
-ldr r4, =0xfffff
+ldr r4, =0xfffffff
 loop2:
 sub r4, r4, #1
 cmp r4, #0
